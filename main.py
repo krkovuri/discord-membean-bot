fb2ba2f9f322d3a90152df7fcc62eb5626959da8
krkovuri <krishnaadvaita4@gmail.com>
2024-10-02 11:53:09 -0400

    idk

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Hello World")
+from dotenv import load_dotenv
+import os
+import discord
+
+load_dotenv()
+
+token = str(os.getenv("TOKEN"))
+# print(token)
+bot = discord.Bot()
+
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is ready and online!")
+
+
+@bot.slash_command(name="hello", description="Say hello to the bot")
+async def hello(ctx: discord.ApplicationContext):
+    await ctx.respond("Hey!")
+
+
+bot.run(os.getenv("TOKEN"))  # run the bot with the token
